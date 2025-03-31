2732473ce8c0cd275aa5f7450918cdcbeb11b01b
KimShun <sihyun@sihyuns-MacBook-Air.local>
2025-03-31 22:42:06 +0900

    [new] 퍼즐 기능 80%

--- playschool/lib/src/puzzleGame/puzzle.dart
+++ playschool/lib/src/puzzleGame/puzzle.dart
@@ -1,8 +1,107 @@
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:http/http.dart' as http;
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:playschool/src/common/component/color.dart';
 
-class PuzzleGame extends StatelessWidget {
+class PuzzleGame extends StatefulWidget {
   const PuzzleGame({super.key});
+
+  @override
+  State<PuzzleGame> createState() => _PuzzleGameState();
+}
+
+class _PuzzleGameState extends State<PuzzleGame> {
+  ui.Image? _image;
+  List<ui.Image> _imagePieces = [];
+  Map<int, bool> _placedPieces = {};
+  Map<int, int> _answerMapping = {};
+
+  final String imageUrl = "https://th.bing.com/th/id/OIP.W_jE3Gz7KaX_EeHEbW4FqwAAAA?rs=1&pid=ImgDetMain";
+  bool _puzzleCompleted = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _puzzleCompleted = false;
+    _loadImageUrl();
+  }
+
+  Future<void> _loadImageUrl() async {
+    final response = await http.get(Uri.parse(imageUrl));
+
+    if(response.statusCode == 200) {
+      final Uint8List data = response.bodyBytes;
+      final ui.Codec codec = await ui.instantiateImageCodec(data,
+        targetWidth: 300, targetHeight: 300
+      );
+      final ui.FrameInfo frame = await codec.getNextFrame();
+
+      setState(() {
+        _image = frame.image;
+        _splitImage();
+      });
+    }
+  }
+
+  Future<void> _splitImage() async {
+    if(_image == null) return;
+
+    int imageWidth = _image!.width;
+    int imageHeight = _image!.height;
+
+    int pieceWidth = imageWidth ~/ 2;
+    int pieceHeight = imageHeight ~/ 2;
+
+    List<int> originalIndexes = [0, 1, 2, 3];
+    List<ui.Image> pieces = [];
+
+    Future<void> cutPiece(int left, int top) async {
+      final recorder = ui.PictureRecorder();
+      final canvas = ui.Canvas(recorder, Rect.fromPoints(Offset(0, 0), Offset(pieceWidth.toDouble(), pieceHeight.toDouble())));
+
+      final src = Rect.fromLTWH(left.toDouble(), top.toDouble(), pieceWidth.toDouble(), pieceHeight.toDouble());
+      final dst = Rect.fromLTWH(0, 0, pieceWidth.toDouble(), pieceHeight.toDouble());
+
+      canvas.drawImageRect(_image!, src, dst, Paint());
+      final picture = recorder.endRecording();
+      final img = await picture.toImage(pieceWidth, pieceHeight);
+
+      pieces.add(img);
+    }
+
+    await cutPiece(0, 0);
+    await cutPiece(pieceWidth, 0);
+    await cutPiece(0, pieceHeight);
+    await cutPiece(pieceWidth, pieceHeight);
+
+    List<int> shuffleIndexes = List.from(originalIndexes)..shuffle();
+
+    List<ui.Image> shuffledPieces = List.generate(4, (index) => pieces[shuffleIndexes[index]]);
+    Map<int, int> answerMapping = { for (int i=0; i<4; i++) originalIndexes[i] : shuffleIndexes[i] };
+
+    setState(() {
+      _imagePieces = shuffledPieces;
+      _placedPieces = {0: false, 1: false, 2: false, 3: false};
+      _answerMapping = answerMapping;
+
+      print(originalIndexes);
+      print(shuffleIndexes);
+      print(answerMapping);
+    });
+  }
+
+  void _checkPuzzleCompletion() {
+    if(_placedPieces.values.every((piece) => piece == true)) {
+      setState(() {
+        _puzzleCompleted = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,6 +184,56 @@ class PuzzleGame extends StatelessWidget {
                                         color: PLAY_CARD_COLOR,
                                         border: Border.all(color: PLAY_STROKE_COLOR, width: 2)
                                       ),
+                                      child: DragTarget<int>(
+                                        onAcceptWithDetails: (details) {},
+                                        builder: (context, candidateData, rejectedData) {
+                                          return Stack(
+                                            children: List.generate(_imagePieces.length, (index) {
+                                              int x = (index % 2) * 150;
+                                              int y = (index ~/ 2) * 150;
+
+                                              return Positioned(
+                                                left: x.toDouble(),
+                                                top: y.toDouble(),
+                                                child: DragTarget<int>(
+                                                  onAcceptWithDetails: (details) {
+                                                    int draggedIndex = details.data;
+                                                    int originalIndex = _answerMapping[draggedIndex]!;
+
+                                                    // print("Dragged: $draggedIndex, Should be at: $originalIndex, Dropped at: $index");
+                                                    if(originalIndex == index) {
+                                                      setState(() {
+                                                        _placedPieces[index] = true;
+                                                      });
+                                                    }
+
+                                                    _checkPuzzleCompletion();
+                                                  },
+                                                  builder: (context, candidateData, rejectedData) {
+                                                    if(_placedPieces[index] == true) {
+                                                      return SizedBox(
+                                                        width: 150,
+                                                        height: 150,
+                                                        child: RawImage(
+                                                          image: _imagePieces[_answerMapping.keys.firstWhere((k) => _answerMapping[k] == index)]
+                                                        )
+                                                      );
+                                                    } else {
+                                                      return Container(
+                                                        width: 150,
+                                                        height: 150,
+                                                        decoration: BoxDecoration(
+                                                            border: Border.all(color: Colors.orange, width: 1)
+                                                        ),
+                                                      );
+                                                    }
+                                                  }
+                                                )
+                                              );
+                                            }),
+                                          );
+                                        }
+                                      ),
                                     ),
                                   ],
                                 ),
@@ -127,39 +276,32 @@ class PuzzleGame extends StatelessWidget {
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
-                                Container(
-                                  width: 65,
-                                  height: 65,
-                                  decoration: BoxDecoration(
-                                    color: Color(0xFFD9D9D9),
-                                    border: Border.all(color: Colors.black, width: 1)
-                                  ),
-                                ),
-                                Container(
-                                  width: 65,
-                                  height: 65,
-                                  decoration: BoxDecoration(
-                                      color: Color(0xFFD9D9D9),
-                                      border: Border.all(color: Colors.black, width: 1)
-                                  ),
-                                ),
-                                Container(
-                                  width: 65,
-                                  height: 65,
-                                  decoration: BoxDecoration(
-                                      color: Color(0xFFD9D9D9),
-                                      border: Border.all(color: Colors.black, width: 1)
-                                  ),
-                                ),
-                                Container(
-                                  width: 65,
-                                  height: 65,
-                                  decoration: BoxDecoration(
-                                      color: Color(0xFFD9D9D9),
-                                      border: Border.all(color: Colors.black, width: 1)
-                                  ),
-                                ),
-                              ],
+                                ...List.generate(_imagePieces.length, (index) {
+                                  return Draggable<int>(
+                                    data: index,
+                                    feedback: Container(
+                                      width: 150,
+                                      height: 150,
+                                      child: RawImage(image: _imagePieces[index]),
+                                    ),
+                                    childWhenDragging: Container(
+                                      width: 65,
+                                      height: 65,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(color: Colors.black, width: 1)
+                                      ),
+                                    ),
+                                    child: Container(
+                                      width: 65,
+                                      height: 65,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.black, width: 1),
+                                      ),
+                                      child: RawImage(image: _imagePieces[index]),
+                                    )
+                                  );
+                                })
+                              ]
                             ),
                           ),
                         ),
@@ -199,9 +341,38 @@ class PuzzleGame extends StatelessWidget {
               ),
             ),
           ),
-
+          if (_puzzleCompleted == true)
+            Stack(
+              children: [
+                Opacity(opacity: 0.3, child: Container(color: Colors.black),),
+                Lottie.asset("assets/lottie/fireworks2.json"),
+              ],
+            )
         ],
       ),
     );
+  }
+}
+
+class PuzzlePainter extends CustomPainter {
+  final ui.Image image;
+  final double x, y, width, height;
+
+  PuzzlePainter(this.image, this.x, this.y, this.width, this.height);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+    canvas.drawImageRect(
+      image,
+      Rect.fromLTWH(x, y, width, height),  // 원본 이미지에서 잘라낼 영역
+      Rect.fromLTWH(0, 0, size.width, size.height),  // Container 크기에 맞게 조정
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
