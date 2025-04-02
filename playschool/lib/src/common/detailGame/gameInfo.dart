f0876e7021ba5ae7e504bd3c7f07920849a976b9
KimShun <sihyun@sihyuns-MacBook-Air.local>
2025-04-02 20:12:10 +0900

    [NEW] 게임정보 관리하는 파일생성

---
+++ playschool/lib/src/common/detailGame/gameInfo.dart
@@ -0,0 +1,125 @@
+// 놀이터 게임
+GameData wordCard = GameData(
+  gameIconPath: "assets/icon/cardGame.jpg",
+  name: "단어 맞추기",
+  gameType: GameType.play,
+  shortDetail: "",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "연령에 상관없이 누구나 재미있게 즐길 수 있어요~!",
+  isAvailable: true,
+);
+
+GameData findWrongPicture = GameData(
+  gameIconPath: "assets/icon/wrongPicture.jpg",
+  name: "틀린그림찾기",
+  gameType: GameType.play,
+  shortDetail: "두 그림을 보고, 다른 부분을 찾아봐~!",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "연령에 상관없이 누구나 재미있게 즐길 수 있어요~!",
+  isAvailable: true,
+);
+
+// 예체능 게임
+GameData followDance = GameData(
+  gameIconPath: "assets/icon/dance.jpg",
+  name: "율동 따라하기",
+  gameType: GameType.ent,
+  shortDetail: "",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "연령에 상관없이 누구나 재미있게 즐길 수 있어요~!",
+  isAvailable: true
+);
+
+GameData drawPicture = GameData(
+  gameIconPath: "assets/icon/drawPainting.webp",
+  name: "그림 그리기",
+  gameType: GameType.ent,
+  shortDetail: "",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "연령에 상관없이 누구나 재미있게 즐길 수 있어요~!",
+  isAvailable: true
+);
+
+GameData colorPaper = GameData(
+  gameIconPath: "assets/icon/colorPaper.jpg",
+  name: "색종이 접기",
+  gameType: GameType.ent,
+  shortDetail: "",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "연령에 상관없이 누구나 재미있게 즐길 수 있어요~!",
+  isAvailable: true,
+);
+
+// 창작활동 게임
+GameData makeFTBook = GameData(
+  gameIconPath: "assets/icon/fairyTaleBook.jpg",
+  name: "동화책 만들기",
+  gameType: GameType.make,
+  shortDetail: "",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "유치원 친구들부터 즐길 수 있는 게임이에요~!",
+  isAvailable: true
+);
+
+GameData makeKidSong = GameData(
+  gameIconPath: "assets/icon/kidsSong.jpg",
+  name: "동요 만들기",
+  gameType: GameType.make,
+  shortDetail: "",
+  longDetail: "",
+  detailVideoPath: "",
+  detailImagePath: [],
+  gameLevel: 1,
+  difficultMsg: "유치원 친구들부터 즐길 수 있는 게임이에요~!",
+  isAvailable: true
+);
+
+enum GameType {
+  play, // 놀이
+  ent, // 예체능
+  make // 창작활동
+}
+
+class GameData {
+  final String gameIconPath;
+  final String name;
+  final GameType gameType;
+  final String shortDetail;
+  final String longDetail;
+  final String detailVideoPath;
+  final List<String> detailImagePath;
+  final int gameLevel;
+  final String difficultMsg;
+  final bool isAvailable;
+
+  const GameData({
+    required this.gameIconPath,
+    required this.name,
+    required this.gameType,
+    required this.shortDetail,
+    required this.longDetail,
+    required this.detailVideoPath,
+    required this.detailImagePath,
+    required this.gameLevel,
+    required this.difficultMsg,
+    required this.isAvailable,
+  });
+}
